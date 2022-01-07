2fb18701d5a76d6bf46fb5ab9dc569dc5af36715
Artem L <artuon@gmail.com>
2022-01-07 11:48:39 +0200

    Form dev

--- mui/src/App.js
+++ mui/src/App.js
@@ -2,6 +2,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 import './App.css';
 
 function App() {
@@ -9,10 +10,29 @@ function App() {
     <div className="App">
         <CssBaseline />
         <Container maxWidth="sm">
-          <Box sx={{ display: 'flex', flexDirection: 'column',  justifyContent: 'center', bgcolor: '#cfe8fc', height: '100vh' }}>
-            <Button width="40%" variant="contained" color="primary">
-              Hello World
-            </Button>
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            bgcolor: '#cfe8fc',
+            height: '100vh'
+          }}>
+            <Box
+              component="form"
+              sx={{
+                '& .MuiTextField-root': {m: 1, width: '25ch'},
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField id="first-name" label="First Name" variant="outlined"/>
+              <TextField id="last-name" label="Last Name" variant="outlined"/>
+              <Container sx={{my: 3}} maxWidth="sm">
+                <Button variant="contained" color="primary">
+                  Hello World
+                </Button>
+              </Container>
+            </Box>
           </Box>
         </Container>
     </div>
