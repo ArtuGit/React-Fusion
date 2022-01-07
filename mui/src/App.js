7e797a1cb50e0febfb6d5c6082851268b268d6ea
Artem L <artuon@gmail.com>
2022-01-07 11:06:42 +0200

    Basic form

--- mui/src/App.js
+++ mui/src/App.js
@@ -1,12 +1,20 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
+        <CssBaseline />
+        <Container maxWidth="sm">
+          <Box sx={{ display: 'flex', flexDirection: 'column',  justifyContent: 'center', bgcolor: '#cfe8fc', height: '100vh' }}>
+            <Button width="40%" variant="contained" color="primary">
+              Hello World
+            </Button>
+          </Box>
+        </Container>
     </div>
   );
 }
