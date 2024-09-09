2effd8e8a44711e4fd4a9b3e6e5eac5a0ce81395
Artu <artuon@gmail.com>
2024-09-09 22:45:53 +0200

    Buttons

--- react-demo/src/components/Boxes/Boxes.tsx
+++ react-demo/src/components/Boxes/Boxes.tsx
@@ -1,5 +1,5 @@
 import '../../App.css'
-import { Box } from "../Box/Box.tsx";
+import {Box, BoxActionEnum} from "../Box/Box.tsx";
 import {useState} from "react";
 import {StatLine} from "../StatLine/StatLine.tsx";
 import {StatItem} from "../StatItem/StatItem.tsx";
@@ -13,23 +13,58 @@ export const Boxes = ({ amount }: BoxesProps) => {
     const [boxes, setBoxes] = useState<boolean[]>(new Array(amount).fill(false));
     const [logMessages, setLogMessages] = useState<LogMessage[]>([createLogMessage({ type: 'warning', message: 'Lets start the game!' })]);
 
-    const  setBoxesItem = (index: number, ) => {
+    let gameOver: boolean = false;
+    const  setBoxesItem = (index: number, action: BoxActionEnum) => {
+
+        gameOver = boxes.length === 1 && action === BoxActionEnum.Delete;
+
         setBoxes((prevBoxes) => {
             const newBoxes = [...prevBoxes];
-            newBoxes[index] = !newBoxes[index];
+            switch (action) {
+                case BoxActionEnum.AddLeft:
+                    newBoxes.splice(index, 0, false);
+                    break;
+                case BoxActionEnum.AddRight:
+                    newBoxes.splice(index + 1, 0, false);
+                    break;
+                case BoxActionEnum.TurnOver:
+                    newBoxes[index] = !newBoxes[index];
+                    break;
+                case BoxActionEnum.Delete:
+                    newBoxes.splice(index, 1);
+                    break;
+            }
+            console.log(newBoxes.length);
             return newBoxes;
         });
 
         setLogMessages((prevMessages) => {
             const newMessages = [...prevMessages];
-            newMessages.unshift(createLogMessage({ type: 'info', message: `Box ${index + 1} was ${boxes[index] ? 'disabled' : 'enabled'}` }));
+            switch (action) {
+                case BoxActionEnum.AddLeft:
+                    newMessages.unshift(createLogMessage({ type: 'info', message: `A new box was added to the left of  ${index + 1} box` }));
+                    break;
+                case BoxActionEnum.AddRight:
+                    newMessages.unshift(createLogMessage({ type: 'info', message: `A new box was added to the right of  ${index + 1} box` }));
+                    break;
+                case BoxActionEnum.TurnOver:
+                    newMessages.unshift(createLogMessage({ type: 'info', message: `Box ${index + 1} was ${boxes[index] ? 'disabled' : 'enabled'}` }));
+                    break;
+                case BoxActionEnum.Delete:
+                    newMessages.unshift(createLogMessage({ type: 'info', message: `Box ${index + 1} was deleted` }));
+                    break;
+            }
+            if (gameOver) {
+                newMessages.unshift(createLogMessage({ type: 'warning', message: 'Game over!' }));
+            }
             return newMessages;
         });
     }
 
     const boxesComponents = [];
+    const boxesAmount = (logMessages.length === 1) ? amount : boxes.length;
 
-    for (let i = 0; i < amount; i++) {
+    for (let i = 0; i < boxesAmount; i++) {
         boxesComponents.push(<
             Box key={i} index={i} isSelected={boxes[i]} setBoxesItem={setBoxesItem}
         />);
@@ -41,7 +76,7 @@ export const Boxes = ({ amount }: BoxesProps) => {
                 {boxesComponents}
             </div>
             <StatLine>
-                <StatItem label="Total" value={amount}/>
+                <StatItem label="Total" value={boxesAmount}/>
                 <StatItem label="Selected" value={boxes.filter(isSelected => isSelected).length}/>
             </StatLine>
             <div className="p-4">
