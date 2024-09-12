0fb16ec1744f44c4affa420e4ffddf942f38a8b9
Artu <artuon@gmail.com>
2024-09-12 22:04:01 +0200

    Layout

--- react-demo/src/components/Layout/Layout.tsx
+++ react-demo/src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import '../../App.css'
 import React, {FC, useState} from 'react';
+import {Boxes} from "../Boxes/Boxes.tsx";
 
 export const Layout: FC = () => {
     return (
@@ -9,7 +10,10 @@ export const Layout: FC = () => {
                 <div className="flex justify-between items-center h-full px-40">
                     <div className="flex items-center">
                         <a href="/">
-                            <img src="/rd.svg" alt="Logo" className="h-6 w-6 mr-2" />
+                            {/*<div src="/rd.svg" alt="Logo" className="h-6 w-6 mr-2" />*/}
+                            <div className="font-extrabold text-xl uppercase tracking-wider bg-black text-white px-2 py-1 transform -skew-x-12 inline-block shadow-[4px_4px_0_0_#ff8200]">
+                                REACT DEMO
+                            </div>
                         </a>
                     </div>
                     <div>
@@ -23,13 +27,13 @@ export const Layout: FC = () => {
                     <section className="w-64 flex items-center bg-blue-500">
                         <nav className="w-full">
                             <ul className="py-4">
-                                <li className="px-6 py-2 hover:bg-gray-100 font-bold">
+                                <li className="px-6 py-2 hover:bg-flush-orange font-bold">
                                     <a href="#" className="text-gray-800">Menu Item 1</a>
                                 </li>
-                                <li className="px-6 py-2 hover:bg-gray-100">
+                                <li className="px-6 py-2 hover:bg-flush-orange">
                                     <a href="#" className="text-gray-800">Menu Item 2</a>
                                 </li>
-                                <li className="px-6 py-2 hover:bg-gray-100">
+                                <li className="px-6 py-2 hover:bg-flush-orange">
                                     <a href="#" className="text-gray-800">Menu Item 3</a>
                                 </li>
                             </ul>
@@ -39,7 +43,7 @@ export const Layout: FC = () => {
                     {/* Main content section */}
                     <main className="flex-1 p-8">
                         <h2 className="text-2xl font-bold mb-4">Welcome to React Demo</h2>
-                        {/* Add your main content here */}
+                          <Boxes amount={15}/>
                     </main>
             </div>
             <footer className="bg-flush-orange flex-none h-10 w-full">
