5100f132be4d0839eace6c9de573151c8e5a47f1
Artu <artuon@gmail.com>
2024-10-05 21:06:10 +0200

    Footer

---
+++ react-demo/src/components/layout/Footer/Footer.tsx
@@ -0,0 +1,27 @@
+import React, {FC, useContext} from 'react';
+import {Statistics} from "../CommonStats/Statistics.tsx";
+import {StatContext} from "../../../context/StatisticsProvider.tsx";
+
+export const Footer: FC = () => {
+    const stats = useContext(StatContext);
+
+    return (
+        <footer className="bg-flush-orange flex-none w-full sticky bottom-0">
+            <div className="flex flex-col md:flex-row justify-between items-center px-4 md:px-40 py-2 md:py-0 md:h-16">
+                <Statistics stats={stats}/>
+                <div className="flex flex-col md:flex-row items-center md:space-x-4">
+                    <div className="text-xl font-bold text-white mb-2 md:mb-0">
+                        {/*{currentTime.toLocaleTimeString()}*/} Tine
+                    </div>
+                    <p className="text-sm text-white mb-2 md:mb-0 text-center md:text-left">
+                        All rights reserved © {new Date().getFullYear()}
+                    </p>
+                    <div
+                        className="font-extrabold text-sm uppercase tracking-wider bg-black text-white px-1 py-0.5 transform -skew-x-12 inline-block shadow-[2px_2px_0_0_#ff8200]">
+                        REACT DEMO
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
--- react-demo/src/components/layout/Layout/Layout.tsx
+++ react-demo/src/components/layout/Layout/Layout.tsx
@@ -2,10 +2,9 @@ import '../../../App.css'
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Dialog } from "../../ui/Dialog/Dialog.tsx";
-import { Statistics } from "../CommonStats/Statistics.tsx";
 import { AppContext } from '../../../context/AppProvider.tsx';
 import {MainMenu} from "../MainMenu/MainMenu.tsx";
-import {StatContext} from "../../../context/StatisticsProvider.tsx";
+import {Footer} from "../Footer/Footer.tsx";
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -13,7 +12,6 @@ interface LayoutProps {
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
     const context = useContext(AppContext);
-    const stats = useContext(StatContext);
 
     const { currentTime, handleLoginAction, handleRegisterAction } = context!;
 
@@ -59,22 +57,8 @@ export const Layout: React.FC<LayoutProps> = ({ children }) => {
                 </main>
             </div>
 
-            <footer className="bg-flush-orange flex-none w-full sticky bottom-0">
-                <div className="flex flex-col md:flex-row justify-between items-center px-4 md:px-40 py-2 md:py-0 md:h-16">
-                    <Statistics stats={stats} />
-                    <div className="flex flex-col md:flex-row items-center md:space-x-4">
-                        <div className="text-xl font-bold text-white mb-2 md:mb-0">
-                            {currentTime.toLocaleTimeString()}
-                        </div>
-                        <p className="text-sm text-white mb-2 md:mb-0 text-center md:text-left">
-                            All rights reserved © {new Date().getFullYear()}
-                        </p>
-                        <div className="font-extrabold text-sm uppercase tracking-wider bg-black text-white px-1 py-0.5 transform -skew-x-12 inline-block shadow-[2px_2px_0_0_#ff8200]">
-                            REACT DEMO
-                        </div>
-                    </div>
-                </div>
-            </footer>
+            <Footer/>
+
         </>
     )
 }
