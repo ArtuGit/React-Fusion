4cec6927f1b941079b2878b9a13c89e5397ca8a4
artu <artuon@gmail.com>
2025-04-29 22:17:17 +0200

    feat/header

--- mui/src/components/layout/MainLayout.tsx
+++ mui/src/components/layout/MainLayout.tsx
@@ -1,9 +1,40 @@
 import { ReactNode } from "react";
+import {Box, boxClasses, Container, Typography, useTheme} from "@mui/material";
 
 export const MainLayout = ({ children }: { children?: ReactNode }) => {
+    const theme = useTheme();
     return (
-        <>
-            {children}
-        </>
+        <Box
+            sx={{
+                minHeight: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+                backgroundColor: theme.palette.background.default,
+            }}
+        >
+            <Box
+                component="header"
+                sx={{
+                    width: '100%',
+                    py: 1.5,
+                    px: 3,
+                    backgroundColor: theme.palette.primary.main,
+                    color: theme.palette.primary.contrastText,
+                    boxShadow: 1,
+                    zIndex: 10,
+                }}
+            >
+                <Typography variant="h6" component="div">
+                    Word Slider
+                </Typography>
+            </Box>
+            <Box  component="main"
+                  sx={{
+                    flex: 1,
+                    display: 'flex',
+                  }}>
+                {children}
+            </Box>
+        </Box>
     );
 };
--- mui/src/pages/WordLearningPage.tsx
+++ mui/src/pages/WordLearningPage.tsx
@@ -12,28 +12,25 @@ export const WordLearningPage: FC = () => {
 
   if (isLoading || !words) {
     return (
-      <Container maxWidth="lg">
         <Box
           sx={{
-            minHeight: '100%',
-            height: '100vh',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
+              minHeight: '100%',
+              flex: 1,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
           }}
         >
           <CircularProgress size={60} />
         </Box>
-      </Container>
     );
   }
 
   return (
-    <Container maxWidth="lg">
       <Box
         sx={{
-          minHeight: '100%',
-          height: '100vh',
+          minHeight: '500px',
+          flex: 1,
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
@@ -41,7 +38,6 @@ export const WordLearningPage: FC = () => {
       >
         <WordSlider words={words} />
       </Box>
-    </Container>
   );
 };
 
