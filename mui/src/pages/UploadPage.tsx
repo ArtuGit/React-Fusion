bee70ebd7cdd76ca3ed7262487d4c53132368264
artu <artuon@gmail.com>
2025-05-23 20:59:23 +0200

    Upload Page

---
+++ mui/src/pages/UploadPage.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import { Box, Container } from '@mui/material';
+
+export const UploadPage: FC = () => {
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        Upload Page (work in progress)
+      </Box>
+    </Container>
+  );
+};
+
+export default UploadPage;
