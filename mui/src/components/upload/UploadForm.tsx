3877812f00376764ea9a6923bc62bea7a0391892
artu <artuon@gmail.com>
2025-05-26 11:45:44 +0200

    Update UploadForm component with placeholder content and styling

--- mui/src/components/upload/UploadForm.tsx
+++ mui/src/components/upload/UploadForm.tsx
@@ -1,7 +1,20 @@
+import { Paper } from '@mui/material';
 import { FC } from 'react';
 
 const UploadForm: FC = () => {
-  return <div>Will ne here!</div>;
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        p: 4,
+        width: '100%',
+        bgcolor: 'background.paper',
+        color: 'text.primary',
+      }}
+    >
+      Work in progress. Please check back later for the upload functionality.
+    </Paper>
+  );
 };
 
 export default UploadForm;
